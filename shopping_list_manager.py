b27118b12b3bddedff6c5d93fd72b687cbfb44e8
Abreham Asefa <abrehamasefa49@gmail.com>
2026-02-21 13:03:39 +0300

    Day 5: Shopping Manager List Mini-project

---
+++ shopping_list_manager.py
@@ -0,0 +1,35 @@
+shopping_list = ["Bread","Milk","Vegitables","Fruits"] 
+while True:
+ print("""      press:
+          1. To add an item
+          2. To remove an item
+          3. To veiw a list
+          4. Exit
+          """)
+ item = input(">> ")
+ if item.isdigit():
+    item = int(item)
+    if item == 1:
+        mark = input("Enter the item you want to add: ")
+        shopping_list.append(mark)
+        print("Your item is added sucessfully! ")
+    elif item == 2:
+        arg = input("Enter the value you want to remove: ")
+        if arg in shopping_list:
+            print(shopping_list.remove(arg))
+            print("Your item is successfully removed! ")
+        else:
+            print("Not Found! ")
+    elif item == 3:
+        print('\nCurrent lists')
+        for i in shopping_list:
+            print(i)
+    elif item == 4:
+        break
+    else:
+        print("I don't understand that... ")
+ else:
+     print("Invalid input!. Numbers only: ") 
+     
+
+
