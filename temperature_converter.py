c4beca20e642656a52135e11204ebc650c4e03c3
Abreham Asefa <abrehamasefa49@gmail.com>
2026-02-21 13:21:53 +0300

    Day 2: Temperature Converter Update

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,11 +1,14 @@
-# temprature converter
+# temperature converter
 temperature_celsius = float(input(" Enter the degree celsius: "))
+temperature_kelvin = temperature_celsius + 273.15
+temperature_fahrenheit = (temperature_celsius* 9/5) + 32
 if temperature_celsius < -273.15:
-   
-   temperature_kelvin = temperature_celsius + 273.15
-
-   temperature_fahrenheit = (temperature_celsius* 9/5) + 32
-
-   print(f"temperature in kelvin = {temperature_kelvin:.2f}")
-   print(f"temperature in fahrenheit = { temperature_fahrenheit:.2f}")
-print("invalid temperature below absolute zero")
+   print("invalid temperature below absolute zero")
+else:
+   temperature = input("Do you want in 'Kelvin'or 'Fahrenheit'? ")
+   if temperature == "Kelvin":
+       print(f"temperature in kelvin = {temperature_kelvin:.2f}")
+   elif temperature == "Fahrenheit":
+       print(f"temperature in fahrenheit = { temperature_fahrenheit:.2f}")
+   else:
+       print("Invalid input")
